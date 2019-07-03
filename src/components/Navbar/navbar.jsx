13bff1e0416b8e34946c490f63c1ffd75e5edd5f
SPDUK <spdevuk@gmail.com>
2019-07-03 23:18:38 +0100

    refactor: use eslint on navbar component

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import "./navbar.scss";
-import logo from "../../images/logo.svg";
-const navbar = () => {
-  return (
-    <nav className="container">
-      <img alt="fylo" src={logo} />
-      <aside>
-        <ul>
-          <li>Features</li>
-          <li>Team</li>
-          <li>Sign In</li>
-        </ul>
-      </aside>
-    </nav>
-  )
-}
+import React from 'react';
+import './navbar.scss';
+import logo from '../../images/logo.svg';
 
-export default navbar
+const navbar = () => (
+  <nav className="container">
+    <img alt="fylo" src={logo} />
+    <aside>
+      <ul>
+        <li>Features</li>
+        <li>Team</li>
+        <li>Sign In</li>
+      </ul>
+    </aside>
+  </nav>
+);
+
+export default navbar;
